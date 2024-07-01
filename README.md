f8eeaeaa5de4c5a23d2b7fa4845a2bf551a9a779
AmanSalman <rapamansalman@gmail.com>
2024-07-01 23:33:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Multi-Section Landing Page
+
+## Project Description
+This project is a multi-section landing page with a dynamically updating navigation menu, scroll-to-top button, collapsible sections, and responsive design.
+
+## Usage
+To use this project, simply open `index.html` in your browser.
+
+## Dependencies
+No external dependencies are required for this project.
+
+## License
+This project is licensed under the MIT License.
---
+++ js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  const Sections = document.querySelectorAll('section');
+  const NavBarList = document.getElementById('navbar-list');
+  
+  Sections.forEach(sec => {
+    const Item = document.createElement('li');
+    const sectionNumber = sec.getAttribute('id');
+    const SecName = sec.getAttribute('data-nav');
+    Item.innerHTML = `<a href="#${sectionNumber}" class="list-link">${SecName}</a>`;
+    NavBarList.appendChild(Item);
+  })
+
+  const links = document.querySelectorAll('.list-link');
+  links.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetSection = document.querySelector(e.target.getAttribute('href'));
+      targetSection.scrollIntoView({ behavior:'smooth' });
+      highlightLink(e);
+    });
+  });
+
+  const highlightLink = (e) => {
+    links.forEach(link => link.classList.remove('active'));
+    e.target.classList.add('active');
+  }
+
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  function toggleScrollToTopButton() {
+      if (window.scrollY > window.innerHeight * 0.6) {
+          scrollToTopBtn.style.display = 'block';
+      } else {
+          scrollToTopBtn.style.display = 'none';
+      }
+  }
+  
+  window.addEventListener('scroll', toggleScrollToTopButton);
+  
+  scrollToTopBtn.addEventListener('click', () => {
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  });
+  
+
+});
+
+
